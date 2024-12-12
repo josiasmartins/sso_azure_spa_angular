c646e82afdda074cd3e7e072cef873a31dc15ab5
josias.m.caitano <josias.m.caitano@accenture.com>
2024-12-12 12:10:06 -0300

    fix: implemented SSO in spa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MSAL_GUARD_CONFIG, MsalBroadcastConfiguration, MsalBroadcastService, MsalGuardConfiguration, MsalService } from '@azure/msal-angular';
-import { ApiService } from './api.service';
-import { filter, Subject, takeUntil } from 'rxjs';
+// app.component.ts
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
+import { MSAL_GUARD_CONFIG, MsalBroadcastService, MsalGuardConfiguration, MsalService } from '@azure/msal-angular';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 import { InteractionStatus, RedirectRequest } from '@azure/msal-browser';
 import { environment } from '../environment/enviroment';
+// import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,97 +13,40 @@ import { environment } from '../environment/enviroment';
   styleUrls: ['./app.component.css'],
   standalone: false
 })
-export class AppComponent implements OnInit {
-  // isAuthenticated = false;
-  // data: any;
-  isUserLoggedIn: boolean = false;
+export class AppComponent implements OnInit, OnDestroy {
+  isUserLoggedIn = false;
   private readonly _destroy = new Subject<void>();
 
   constructor(
     @Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
     private authService: MsalService,
-    private msalBroadCastService: MsalBroadcastService
+    private msalBroadcastService: MsalBroadcastService
   ) {}
 
-  ngOnInit() {
-
-    this.msalBroadCastService.inProgress$.pipe(
-      filter((interactionStatus: InteractionStatus) => 
-        interactionStatus == InteractionStatus.None
-      ),
-      takeUntil(this._destroy)
-    ).subscribe(x => {
-      this.isUserLoggedIn = this.authService.instance.getAllAccounts().length > 0;
-    })
-
-
-    // this.msalService.instance.handleRedirectPromise().then((response) => {
-    //   if (response && response.account) {
-    //     this.msalService.instance.setActiveAccount(response.account);
-    //     this.isAuthenticated = true;
-    //   } else if (this.msalService.instance.getAllAccounts().length > 0) {
-    //     this.isAuthenticated = true;
-    //   }
-    // }).catch((error) => {
-    //   console.error('Erro ao processar handleRedirectPromise:', error);
-    // });
+  async ngOnInit() {
+    await this.authService.instance.initialize();
+    // Subscribe to MSAL's interaction status updates
+    this.msalBroadcastService.inProgress$
+      .pipe(
+        filter((status: InteractionStatus) => status === InteractionStatus.None),
+        takeUntil(this._destroy)
+      )
+      .subscribe(() => {
+        this.isUserLoggedIn = this.authService.instance.getAllAccounts().length > 0;
+      });
   }
 
-  // login() {
-  //   this.msalService.loginPopup().subscribe({
-  //     next: (response) => {
-  //       this.isAuthenticated = true;
-  //       console.log('Login realizado com sucesso!', response);
-  //       this.msalService.instance.setActiveAccount(response.account); // Define a conta ativa
-  //     },
-  //     error: (error) => {
-  //       console.error('Erro no login:', error);
-  //     },
-  //   });
-  // }
-
-  // getProtectedData() {
-  //   this.msalService
-  //     .acquireTokenSilent({
-  //       scopes: ['api://425cae19-a568-407c-a25f-91f7fb243070/access_as_user'], // Substitua pelos escopos configurados no Azure AD
-  //     })
-  //     .subscribe({
-  //       next: (response) => {
-  //         const accessToken = response.accessToken;
-  //         console.log('Token obtido:', accessToken);
-
-  //         this.apiService.getProtectedData(accessToken).subscribe({
-  //           next: (data) => {
-  //             this.data = data;
-  //             console.log('Dados protegidos recebidos:', data);
-  //           },
-  //           error: (error) => {
-  //             console.error('Erro ao acessar dados protegidos:', error);
-  //           },
-  //         });
-  //       },
-  //       error: (error) => {
-  //         console.error('Erro ao obter token:', error);
-  //       },
-  //     });
-  // }
-
   ngOnDestroy(): void {
-    this._destroy.next(undefined);
+    this._destroy.next();
     this._destroy.complete();
   }
 
-  login() {
-    if (this.msalGuardConfig.authRequest) {
-      this.authService.loginRedirect({ ...this.msalGuardConfig.authRequest } as RedirectRequest)
-    } else {
-      this.authService.loginRedirect();
-    }
+  login(): void {
+    const loginRequest = this.msalGuardConfig.authRequest || {};
+    this.authService.loginRedirect({ ...loginRequest } as RedirectRequest);
   }
 
-  logout() {
+  logout(): void {
     this.authService.logoutRedirect({ postLogoutRedirectUri: environment.postLogoUrl });
-    
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,79 +1,62 @@
+// app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { MsalModule, MSAL_INSTANCE, MsalService, MsalInterceptor, MSAL_GUARD_CONFIG, MsalGuard } from '@azure/msal-angular';
+import { MsalModule, MsalService, MsalInterceptor, MsalGuard, MSAL_INSTANCE, MSAL_GUARD_CONFIG, MsalGuardConfiguration } from '@azure/msal-angular';
 import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
-import { CommonModule } from '@angular/common';
-
-// const isIE = window && window.document;
 
 export function MSALInstanceFactory() {
   return new PublicClientApplication({
     auth: {
-      clientId: '425cae19-a568-407c-a25f-91f7fb243070',  // Substitua pelo seu Client ID do Azure AD
-      authority: 'https://login.microsoftonline.com/9d64dfab-644d-4ee5-9620-934b52f48ab5',  // Substitua pelo seu Tenant ID do Azure AD
-      redirectUri: 'http://localhost:4200',  // O URL de redirecionamento após login
+      clientId: '425cae19-a568-407c-a25f-91f7fb243070',
+      authority: 'https://login.microsoftonline.com/9d64dfab-644d-4ee5-9620-934b52f48ab5',
+      redirectUri: 'http://localhost:4200',
     },
     cache: {
       cacheLocation: 'localStorage',
-      // storeAuthStateInCookie: isIE,  // Para browsers antigos como IE
+      storeAuthStateInCookie: false,
     },
   });
+}
+
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return {
+    interactionType: InteractionType.Redirect,
+    authRequest: {
+      scopes: ['user.read'],
+    },
+  };
 }
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    CommonModule, 
-    RouterModule.forRoot(routes), 
-    BrowserModule, 
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
     MsalModule.forRoot(
-      new PublicClientApplication(
-        {
-          auth: {
-            clientId: '425cae19-a568-407c-a25f-91f7fb243070',
-            redirectUri: 'http://localhost:4200',
-            authority: 'https://login.microsoftonline.com/9d64dfab-644d-4ee5-9620-934b52f48ab5'
-          },
-          cache: {
-            cacheLocation: 'localStorage',
-            // storeAuthStateInCookie: isIE
-          }
-        }
-      ),
+      MSALInstanceFactory(),
+      MSALGuardConfigFactory(),
       {
         interactionType: InteractionType.Redirect,
-        authRequest: {
-          scopes: ['user.read']
-        }
-      },
-      {
-        interactionType: InteractionType.Redirect,
-        protectedResourceMap: new Map(
-          [
-            ["https://graph.microsoft.com/v1.0/me", ['user.Read']]
-          ]
-        )
+        protectedResourceMap: new Map([
+          ['https://graph.microsoft.com/v1.0/me', ['user.read']],
+        ]),
       }
-    )
+    ),
   ],
   providers: [
-    // {
-    //   provide: MSAL_INSTANCE,
-    //   useFactory: MSALInstanceFactory,
-    // },
-    // MsalService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: MsalInterceptor,
-      multi: true
+      multi: true,
     },
-    MsalGuard
+    MsalGuard,
+    MsalService,
   ],
-  exports: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
